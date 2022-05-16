f78da83cfdcbfade2ff72c9ed891158a795541eb
wooyoung0903 <wooyoung@aliseon.com>
2022-05-16 14:32:01 +0900

    example header 페이지 생성

---
+++ layout/header.js
@@ -0,0 +1,9 @@
+// export default function Header() {
+//   return <header>헤더입니다.</header>;
+// }
+
+const Header = () => {
+  return <div>ggggg</div>;
+};
+
+export default Header;
---
+++ pages/example.js
@@ -0,0 +1,9 @@
+import Header from "../layout/header";
+
+export default function Example() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
